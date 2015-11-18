7736b4e45208bb5b435d23bd917050f859c98126
Andrej Švec <andy.swec@gmail.com>
2015-11-18 23:59:52 +0100

    Let the filter stay on top when scrolling

--- js/custom.js
+++ js/custom.js
@@ -24,3 +24,29 @@ function nonlinear(slider_value) {
   }
   return String(return_value);
 }
+
+$(function() {
+
+  setTimeout(function() {
+    filterHeight = $('#filter').outerHeight(false);
+    orderHeight = $('.section#order').outerHeight(false);
+    console.log(filterHeight);
+    console.log(orderHeight)
+  }, 10);
+
+  var elementPosition = $('#filter').offset();
+  console.log(elementPosition);
+
+  $(window).scroll(function() {
+    if($(window).scrollTop() >= elementPosition.top){
+      $('#filter').css({position: 'fixed', top: 0});
+      $('#order').css({position: 'fixed', top: filterHeight});
+      $('.section#list').css('margin-top', filterHeight + orderHeight);
+    }
+    else {
+      $('.section#filter').css('position','static');
+      $('.section#order').css('position','static');
+      $('.section#list').css('margin-top', 0);
+    }
+  });
+});
